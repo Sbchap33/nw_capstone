4914ab955b3f69ceb5026948db07f420c0d8b51e
sbchap33 <ec2-user@ip-172-31-24-164.us-east-2.compute.internal>
2020-05-14 01:16:24 +0000

    add changes

---
+++ usgs_api_pull_funct.py
@@ -0,0 +1,67 @@
+# Base API pull for USGS # 
+
+import requests
+import json
+import flask
+import logging
+
+def main(request):
+    try:
+        request = request.get_json() # handler method for flask object to Json
+        
+        # API specific inputs to function
+        url = "https://waterservices.usgs.gov/nwis/iv/?format=json&sites=09058000&parameterCd=00060,00065&siteStatus=all"
+        
+        #Call the data function
+        data = request_historical_data(url)
+        state=data["state"]
+        response = assemble_response_json(data,state)
+        headers ={"Content-Type": "application/json"}
+        return flask.make_response(response,200,headers)
+        
+    except Exception as e:
+        logging.error("Exception occurred", exc_info=True)
+        
+
+def request_historical_data(url_n):
+    r = requests.request("GET", url_n)
+    r.encoding= 'JSON'
+    r_json = json.loads(r.text)
+    r_dumps=json.dumps(r_json, indent=4)
+    ######### date time ######## 
+    time=r_json["value"]["queryInfo"]["note"][3]["value"]
+    #update state with new time# 
+    state=time
+    #print(time)
+    ########## site name ##########
+    site_name=r_json["value"]["timeSeries"][0]["sourceInfo"]["siteName"]
+    #print(site_name)
+    ######### cfs ########
+    cfs=r_json["value"]["timeSeries"][0]["values"][0]["value"][0]["value"]
+    cfs=int(cfs)
+    #print(cfs)
+    ## organize into one tuple ## 
+    data = {"state":time, "site_name":site_name, "cfs": cfs}
+    return data
+    #print(data)
+
+######## function to organize response ###### 
+def assemble_response_json(data_up, state_up):
+    response_dict ={
+        "state": {
+            "upper_c": state_up
+        },
+        "insert": {
+            "upper_c": 
+                data_up
+            },
+        "schema": {
+            "upper_c": {
+                "primary_key": "state"
+            }
+        },
+        "hasMore": False
+    }
+    print(json.dumps(response_dict))
+    #return json.dumps(response_dict)
+
