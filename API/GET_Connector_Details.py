b8f89f97bcdc5e665435d9543c5c78ab21cf373a
Sbchap33 <60025118+Sbchap33@users.noreply.github.com>
2020-06-10 09:52:42 -0600

    Update GET_Connector_Details.py

--- API/GET_Connector_Details.py
+++ API/GET_Connector_Details.py
@@ -2,9 +2,9 @@ import requests
 import json
 import yaml
 
-url = "https://api.fivetran.com/v1/connectors/pampers_overnight"
+url = "https://api.fivetran.com/v1/connectors/{connector id}"
 
-conf = yaml.load(open('/home/ec2-user/environment/nw_capstone/API/Requirements.yml'))
+conf = yaml.load(open('{User Directory}/Requirements.yml'))
 basic = conf['key']['basic']
 
 payload  = {}
